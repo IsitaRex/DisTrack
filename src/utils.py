bbe56c1b441539a8eaa36815c6648d4ef2579581
IsitaRex <ec24819@qmul.ac.uk>
2025-07-10 03:09:47 -0500

    Clean utils.py

--- src/utils.py
+++ src/utils.py
@@ -42,81 +42,6 @@ def get_network(model, channel, num_classes, im_size=(32, 32), embedding_size = 
 def get_time():
     return str(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()))
 
-def distance_wb(gwr, gws):
-    shape = gwr.shape
-    if len(shape) == 4: # conv, out*in*h*w
-        gwr = gwr.reshape(shape[0], shape[1] * shape[2] * shape[3])
-        gws = gws.reshape(shape[0], shape[1] * shape[2] * shape[3])
-    elif len(shape) == 3:  # layernorm, C*h*w
-        gwr = gwr.reshape(shape[0], shape[1] * shape[2])
-        gws = gws.reshape(shape[0], shape[1] * shape[2])
-    elif len(shape) == 2: # linear, out*in
-        tmp = 'do nothing'
-    elif len(shape) == 1: # batchnorm/instancenorm, C; groupnorm x, bias
-        gwr = gwr.reshape(1, shape[0])
-        gws = gws.reshape(1, shape[0])
-        return torch.tensor(0, dtype=torch.float, device=gwr.device)
-
-    dis_weight = torch.sum(1 - torch.sum(gwr * gws, dim=-1) / (torch.norm(gwr, dim=-1) * torch.norm(gws, dim=-1) + 0.000001))
-    dis = dis_weight
-    return dis
-
-def match_loss(gw_syn, gw_real, args):
-    dis = torch.tensor(0.0).to(args.device)
-
-    if args.dis_metric == 'ours':
-        for ig in range(len(gw_real)):
-            gwr = gw_real[ig]
-            gws = gw_syn[ig]
-            dis += distance_wb(gwr, gws)
-
-    elif args.dis_metric == 'mse':
-        gw_real_vec = []
-        gw_syn_vec = []
-        for ig in range(len(gw_real)):
-            gw_real_vec.append(gw_real[ig].reshape((-1)))
-            gw_syn_vec.append(gw_syn[ig].reshape((-1)))
-        gw_real_vec = torch.cat(gw_real_vec, dim=0)
-        gw_syn_vec = torch.cat(gw_syn_vec, dim=0)
-        dis = torch.sum((gw_syn_vec - gw_real_vec)**2)
-
-    elif args.dis_metric == 'cos':
-        gw_real_vec = []
-        gw_syn_vec = []
-        for ig in range(len(gw_real)):
-            gw_real_vec.append(gw_real[ig].reshape((-1)))
-            gw_syn_vec.append(gw_syn[ig].reshape((-1)))
-        gw_real_vec = torch.cat(gw_real_vec, dim=0)
-        gw_syn_vec = torch.cat(gw_syn_vec, dim=0)
-        dis = 1 - torch.sum(gw_real_vec * gw_syn_vec, dim=-1) / (torch.norm(gw_real_vec, dim=-1) * torch.norm(gw_syn_vec, dim=-1) + 0.000001)
-
-    else:
-        exit('unknown distance function: %s'%args.dis_metric)
-
-    return dis
-
-
-def get_loops(ipc):
-    # Get the two hyper-parameters of outer-loop and inner-loop.
-    # The following values are empirically good.
-    if ipc == 1:
-        outer_loop, inner_loop = 1, 1
-    elif ipc == 10:
-        outer_loop, inner_loop = 10, 50
-    elif ipc == 20:
-        outer_loop, inner_loop = 20, 25
-    elif ipc == 30:
-        outer_loop, inner_loop = 30, 20
-    elif ipc == 40:
-        outer_loop, inner_loop = 40, 15
-    elif ipc == 50:
-        outer_loop, inner_loop = 50, 10
-    else:
-        outer_loop, inner_loop = 0, 0
-        exit('loop hyper-parameters are not defined for %d ipc'%ipc)
-    return outer_loop, inner_loop
-
-
 
 def epoch(mode, dataloader, net, optimizer, criterion, args, aug):
     loss_avg, acc_avg, num_exp = 0, 0, 0
@@ -140,7 +65,7 @@ def epoch(mode, dataloader, net, optimizer, criterion, args, aug):
             output = net(img)
         except:
             breakpoint()
-        
+    
         loss = criterion(output, lab)
         acc = np.sum(np.equal(np.argmax(output.cpu().data.numpy(), axis=-1), lab.cpu().data.numpy()))
 
