19126ff0a5c6bf9584cd9f0a4a5d155545556e16
IsitaRex <ec24819@qmul.ac.uk>
2025-07-23 17:06:59 +0100

    Clean DM code

--- DistributionMatching.py
+++ DistributionMatching.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 from torchvision.utils import save_image
 from src.datasets import get_dataset
-from src.utils import get_loops, get_network, evaluate_synset, match_loss, get_time, info_nce_loss, sample_class_data, sample_negative_samples
+from src.utils import  get_network, evaluate_synset, get_time, info_nce_loss, sample_class_data, sample_negative_samples
 
 # Intentar importar wandb de manera segura
 #clear mps cache
@@ -18,11 +18,11 @@ torch.mps.empty_cache()
 def main():
 
     parser = argparse.ArgumentParser(description='Parameter Processing')
-    parser.add_argument('--dataset', type=str, default='GTZAN', help='dataset')
+    parser.add_argument('--dataset', type=str, default='AUDIO_MNIST', help='dataset')
     parser.add_argument('--model', type=str, default='SimplifiedConvNet', help='model')
     parser.add_argument('--ipc', type=int, default=10, help='image(s) per class')
     parser.add_argument('--num_exp', type=int, default=1, help='the number of experiments')
-    parser.add_argument('--num_eval', type=int, default=1, help='the number of evaluating randomly initialized models')
+    parser.add_argument('--num_eval', type=int, default=10, help='the number of evaluating randomly initialized models')
     parser.add_argument('--epoch_eval_train', type=int, default=100, help='epochs to train a model with synthetic data') # it can be small for speeding up with little performance drop
     parser.add_argument('--Iteration', type=int, default=1000, help='training iterations')
     parser.add_argument('--lr_img', type=float, default=2.0, help='learning rate for updating synthetic images')
@@ -36,7 +36,7 @@ def main():
     parser.add_argument('--contrastive_weight', type=float, default=0.2, help='Weight for contrastive loss')
 
     args = parser.parse_args()
-    args.outer_loop, args.inner_loop = get_loops(args.ipc)
+    args.method = 'DM'
     # args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
     args.device = 'mps'
     USE_WANDB = args.use_wandb
--- src/utils.py
+++ src/utils.py
@@ -38,85 +38,8 @@ def get_network(model, channel, num_classes, im_size=(32, 32), embedding_size = 
 
     return net
 
-
 def get_time():
     return str(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()))
-
-def distance_wb(gwr, gws):
-    shape = gwr.shape
-    if len(shape) == 4: # conv, out*in*h*w
-        gwr = gwr.reshape(shape[0], shape[1] * shape[2] * shape[3])
-        gws = gws.reshape(shape[0], shape[1] * shape[2] * shape[3])
-    elif len(shape) == 3:  # layernorm, C*h*w
-        gwr = gwr.reshape(shape[0], shape[1] * shape[2])
-        gws = gws.reshape(shape[0], shape[1] * shape[2])
-    elif len(shape) == 2: # linear, out*in
-        tmp = 'do nothing'
-    elif len(shape) == 1: # batchnorm/instancenorm, C; groupnorm x, bias
-        gwr = gwr.reshape(1, shape[0])
-        gws = gws.reshape(1, shape[0])
-        return torch.tensor(0, dtype=torch.float, device=gwr.device)
-
-    dis_weight = torch.sum(1 - torch.sum(gwr * gws, dim=-1) / (torch.norm(gwr, dim=-1) * torch.norm(gws, dim=-1) + 0.000001))
-    dis = dis_weight
-    return dis
-
-def match_loss(gw_syn, gw_real, args):
-    dis = torch.tensor(0.0).to(args.device)
-
-    if args.dis_metric == 'ours':
-        for ig in range(len(gw_real)):
-            gwr = gw_real[ig]
-            gws = gw_syn[ig]
-            dis += distance_wb(gwr, gws)
-
-    elif args.dis_metric == 'mse':
-        gw_real_vec = []
-        gw_syn_vec = []
-        for ig in range(len(gw_real)):
-            gw_real_vec.append(gw_real[ig].reshape((-1)))
-            gw_syn_vec.append(gw_syn[ig].reshape((-1)))
-        gw_real_vec = torch.cat(gw_real_vec, dim=0)
-        gw_syn_vec = torch.cat(gw_syn_vec, dim=0)
-        dis = torch.sum((gw_syn_vec - gw_real_vec)**2)
-
-    elif args.dis_metric == 'cos':
-        gw_real_vec = []
-        gw_syn_vec = []
-        for ig in range(len(gw_real)):
-            gw_real_vec.append(gw_real[ig].reshape((-1)))
-            gw_syn_vec.append(gw_syn[ig].reshape((-1)))
-        gw_real_vec = torch.cat(gw_real_vec, dim=0)
-        gw_syn_vec = torch.cat(gw_syn_vec, dim=0)
-        dis = 1 - torch.sum(gw_real_vec * gw_syn_vec, dim=-1) / (torch.norm(gw_real_vec, dim=-1) * torch.norm(gw_syn_vec, dim=-1) + 0.000001)
-
-    else:
-        exit('unknown distance function: %s'%args.dis_metric)
-
-    return dis
-
-
-def get_loops(ipc):
-    # Get the two hyper-parameters of outer-loop and inner-loop.
-    # The following values are empirically good.
-    if ipc == 1:
-        outer_loop, inner_loop = 1, 1
-    elif ipc == 10:
-        outer_loop, inner_loop = 10, 50
-    elif ipc == 20:
-        outer_loop, inner_loop = 20, 25
-    elif ipc == 30:
-        outer_loop, inner_loop = 30, 20
-    elif ipc == 40:
-        outer_loop, inner_loop = 40, 15
-    elif ipc == 50:
-        outer_loop, inner_loop = 50, 10
-    else:
-        outer_loop, inner_loop = 0, 0
-        exit('loop hyper-parameters are not defined for %d ipc'%ipc)
-    return outer_loop, inner_loop
-
-
 
 def epoch(mode, dataloader, net, optimizer, criterion, args, aug):
     loss_avg, acc_avg, num_exp = 0, 0, 0
@@ -292,19 +215,3 @@ def sample_negative_samples(class_id, num_samples, indices_class, all_real_data)
     sampled_indices = np.random.choice(negative_indices, size=num_samples, replace=False)
     # Return the sampled data points
     return all_real_data[sampled_indices]
-
-def load_AST():
-  config = ASTConfig(max_length=128)
-  AST_SAMPLE_RATE = 16000
-  feature_extractor = ASTFeatureExtractor.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593", config=config)
-  feature_extractor_orig = ASTFeatureExtractor.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593")
-  model_orig = ASTModel.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593")
-  model = ASTModel.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593", config=config,ignore_mismatched_sizes=True)
-  model.embeddings.position_embeddings = torch.nn.Parameter(model_orig.embeddings.position_embeddings[0,:146,:])
-
-  return model
-
-def load_AST_feature_extractor():
-  config = ASTConfig(max_length=128)
-  feature_extractor = ASTFeatureExtractor.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593", config=config)
-  return feature_extractor
